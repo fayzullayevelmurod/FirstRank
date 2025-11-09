f87af670e507130e47d5046ac2bb4ade477b8009
QobilovSardor <note47297@gmail.com>
2025-11-09 21:27:05 +0500

    desctop finished

---
+++ js/main.js
@@ -0,0 +1,144 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // our-team-swiper
+  const teamSwiper = new Swiper(".our-team-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  // successful__cases-swiper
+  const successfulCasesSwiper = new Swiper(".successful__cases-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".next-swiper-btn",
+      prevEl: ".prev-swiper-btn",
+    },
+  });
+
+  // reviews__certificate-swiper
+  const reviewsCertificateSwiper = new Swiper(".reviews__certificate-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".reviews__certificate-swiper-next",
+      prevEl: ".reviews__certificate-swiper-prev",
+    },
+  });
+
+  // system-approach__swiper
+  const systemApproachSwiper = new Swiper(".system-approach__swiper", {
+    slidesPerView: 2.8,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".system-approach__swiper-next",
+      prevEl: ".system-approach__swiper-prev",
+    },
+  });
+
+  // accordion
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions[0].classList.add("active");
+  accordions[0].querySelector(".accordion-content").style.maxHeight =
+    accordions[0].querySelector(".accordion-content").scrollHeight + "px";
+
+  accordions.forEach((accordion) => {
+    const header = accordion.querySelector(".accordion-header");
+
+    header.addEventListener("click", () => {
+      accordions.forEach((acc) => {
+        if (acc !== accordion) {
+          acc.classList.remove("active");
+          acc.querySelector(".accordion-content").style.maxHeight = null;
+          acc.querySelector(".accordion-arrow").style.transform =
+            "rotate(0deg)";
+        }
+      });
+
+      accordion.classList.toggle("active");
+
+      const content = accordion.querySelector(".accordion-content");
+      const arrow = accordion.querySelector(".accordion-arrow");
+
+      if (accordion.classList.contains("active")) {
+        content.style.maxHeight = content.scrollHeight + "px";
+        arrow.style.transform = "rotate(90deg)";
+      } else {
+        content.style.maxHeight = null;
+        arrow.style.transform = "rotate(0deg)";
+      }
+    });
+  });
+
+  // promotion lists
+  const items = document.querySelectorAll(".promotions__list-item");
+  const button = document.querySelector(".show__all-list");
+
+  let isExpanded = false;
+
+  function updateVisibleItems() {
+    const isMobile = window.innerWidth <= 768;
+    const limit = isMobile ? 14 : 18;
+
+    if (!isExpanded) {
+      items.forEach((item, index) => {
+        if (index < limit) {
+          item.classList.remove("hidden");
+        } else {
+          item.classList.add("hidden");
+        }
+      });
+    } else {
+      items.forEach((item) => item.classList.remove("hidden"));
+    }
+
+    if (items.length <= limit) {
+      button.style.display = "none";
+    } else {
+      button.style.display = "inline-block";
+    }
+
+    button.textContent = isExpanded
+      ? "Свернуть список"
+      : "Смотреть весь список";
+  }
+
+  updateVisibleItems();
+
+  window.addEventListener("resize", updateVisibleItems);
+
+  button.addEventListener("click", () => {
+    isExpanded = !isExpanded;
+    updateVisibleItems();
+  });
+});
